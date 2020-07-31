148e0bbee7c7b42648255dd057e033361e319ce7
iy06 <yuki.inoue@di-v.co.jp>
2020-07-31 19:07:42 +0900

    edit files

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+$(function(){
 
-
+  $(".member-image").hover(function(){
+    console.log(1)
+    $("#member-image-before").css('display','none')
+    $("#member-image-after").css('display','block')
+  },function(){
+    $("#member-image-before").css('display','block')
+    $("#member-image-after").css('display','none')
+  })
+})
